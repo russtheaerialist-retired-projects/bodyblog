8627a943d1aee336de1a4e0b9545486ecc57ceb3
Russell Hay <russell.hay@gmail.com>
2010-01-01 22:18:29 -0800

    Adding Capistrano files

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "bodyblog"
+set :repository,  "git://github.com/RussTheAerialist/bodyblog.git"
+set :scm, :git
+
+set :deploy_to, "/home/rhay/sites/bodyblog"
+set :use_sudo, false
+
+role :web, "localhost"                          # Your HTTP server, Apache/etc
+role :app, "localhost"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true # This is where Rails migrations will run
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
