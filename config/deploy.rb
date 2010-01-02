d53d98613818219f4b76a75bf17fc7edc8d1c1ce
Russell Hay <russell.hay@gmail.com>
2010-01-02 12:12:48 -0800

    enabling passanger

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,10 +13,10 @@ role :db,  "localhost", :primary => true # This is where Rails migrations will r
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
