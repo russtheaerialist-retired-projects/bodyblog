c4397eec71f1274ea49f3c14a268ca17cc2ece6f
Russell Hay <russell.hay@gmail.com>
2010-01-03 11:30:48 -0800

    Adding google charts, requirement for sparklines, and the soon-to-be-implemented charts controller

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "fleximage"
   config.gem 'BlueCloth', :lib => "bluecloth", :version => '1.0.1'
+  config.gem 'sparklines'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
